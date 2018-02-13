825377e7ded464e6fa2553606c3dd68d328ddff1
jack <jack.nevitt@dwellant.com>
2018-02-13 13:58:17 +0000

    removed rules for linting certain files

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,5 +1,4 @@
-
-
+/* eslint-disable global-require */
 const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
@@ -74,9 +73,8 @@ module.exports = {
     // We placed these paths second because we want `node_modules` to "win"
     // if there are any conflicts. This matches Node resolution mechanism.
     // https://github.com/facebookincubator/create-react-app/issues/253
-    modules: ['node_modules', paths.appNodeModules].concat(
-      // It is guaranteed to exist because we tweak it in `env.js`
-      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)),
+    modules: ['node_modules', paths.appNodeModules].concat(process.env.NODE_PATH.split(path.delimiter).filter(Boolean)),
+    // It is guaranteed to exist because we tweak it in `env.js`
     // These are the reasonable defaults supported by the Node ecosystem.
     // We also include JSX as a common component filename extension to support
     // some tools, although we do not recommend using it, see:
